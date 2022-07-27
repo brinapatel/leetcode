3aa7ca96cb94f7fef9bc2be2ece3ec19d4f68b8b
Brina Patel <brina.patel@medly.com>
2022-07-27 01:11:14 -0400

    2022-07-26

--- Codes/Leetcode/180__consecutive_number.sql
+++ Codes/Leetcode/180__consecutive_number.sql
@@ -6,3 +6,15 @@ Logs l3
 where l1.id = (l2.id+1) and
  l1.id = (l3.id+2) and l1.num = l2.num
  and l2.num = l3.num
+
+ ---------------------------------------------------
+
+
+WITH cte_1 AS 
+(SELECT num, 
+        LEAD(num) OVER(order by id) as next,
+        LAG(num) OVER(order by id) as prev
+FROM Logs)
+SELECT DISTINCT num AS ConsecutiveNums 
+FROM cte_1 WHERE
+num = next and next = prev
